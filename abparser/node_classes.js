e8a9763bec6313226e547e25be4e236ae76e20cd
sattirajus <srikanthsattiraju@gmail.com>
2025-03-07 21:09:58 +0530

    Edit a comment to fix a documentation mistake

--- abparser/node_classes.js
+++ abparser/node_classes.js
@@ -6,7 +6,8 @@ class Node {
     }
 };
 
-// An instance of this class is assigned as the value of a node.
+// An instance of this class is assigned to array elements in an expression
+// node's value array.
 class Operation {
     constructor(leftOperand, operator, rightOperand) {
         this.leftOperand = leftOperand;
