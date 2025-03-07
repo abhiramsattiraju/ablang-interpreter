87967d1acba94cb6d472231855e2613a34812f2c
sattirajus <srikanthsattiraju@gmail.com>
2025-03-07 21:31:09 +0530

    Runner can print strings

--- runner.js
+++ runner.js
@@ -1,5 +1,6 @@
 const { NODE_TYPE_PRINT_STATEMENT, NODE_TYPE_NUMBER, NODE_TYPE_STRING } = require("./abparser/ast_node_types");
 const exceptions = require("./exceptions");
+const operator_types = require("./abparser/operator_types");
 
 // Run an ABLang AST
 function run(ast) {
@@ -15,7 +16,17 @@ function run(ast) {
 }
 
 function evaluate(expression) {
-    return expression;
+    let result;
+
+    expression.value.forEach((operation, index) => {
+        if(operation.operator == operator_types.LEAVE_AS_IS) {
+            result = operation.leftOperand;
+        } else {
+            result = null;
+        }
+    });
+
+    return result;
 }
 
 
