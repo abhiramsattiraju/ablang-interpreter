d466aedd40a87d05ecb948dc7685163eee81eda2
sattirajus <srikanthsattiraju@gmail.com>
2025-03-28 10:57:06 +0530

    Lexer passes all the tests

--- tests/lexer.test.js
+++ tests/lexer.test.js
@@ -65,11 +65,8 @@ describe('Lexer Tests', () => {
         ]);
     });
 
-    it('Should lex semicolons correctly', () => {
-        const tokens = lex(';');
-        expect(tokens).toEqual([
-            new Token(tokenTypes.TOKEN_TYPE_SEMICOLON, ';'),
-        ]);
+    it('Should throw an error if a semicolon is found', () => {
+        expect(() => {lex(';')}).toThrow(Error);
     });
 
     it('Should lex strings correctly', () => {
@@ -90,15 +87,14 @@ describe('Lexer Tests', () => {
     });
 
     it('Should lex mixed input correctly', () => {
-        const tokens = lex('print(123 + "hello");');
+        const tokens = lex('print(123 + "hello")');
         expect(tokens).toEqual([
             new Token(tokenTypes.TOKEN_TYPE_KEYWORD, 'print'),
             new Token(tokenTypes.TOKEN_TYPE_ROUND_BRACKET, '('),
             new Token(tokenTypes.TOKEN_TYPE_NUMBER, 123),
             new Token(tokenTypes.TOKEN_TYPE_OPERATOR, '+'),
             new Token(tokenTypes.TOKEN_TYPE_STRING, 'hello'),
-            new Token(tokenTypes.TOKEN_TYPE_ROUND_BRACKET, ')'),
-            new Token(tokenTypes.TOKEN_TYPE_SEMICOLON, ';'),
+            new Token(tokenTypes.TOKEN_TYPE_ROUND_BRACKET, ')')
         ]);
     });
 
@@ -107,25 +103,25 @@ describe('Lexer Tests', () => {
     });
 
     it('Should handle various whitespace combinations', () => {
-        const tokens = lex('  print \t ( \n 123  + "hello" \r ) ;  ');
+        const tokens = lex('  print \t (  123  + "hello" \r )   ');
         expect(tokens).toEqual([
             new Token(tokenTypes.TOKEN_TYPE_KEYWORD, 'print'),
             new Token(tokenTypes.TOKEN_TYPE_ROUND_BRACKET, '('),
             new Token(tokenTypes.TOKEN_TYPE_NUMBER, 123),
             new Token(tokenTypes.TOKEN_TYPE_OPERATOR, '+'),
             new Token(tokenTypes.TOKEN_TYPE_STRING, 'hello'),
-            new Token(tokenTypes.TOKEN_TYPE_ROUND_BRACKET, ')'),
-            new Token(tokenTypes.TOKEN_TYPE_SEMICOLON, ';'),
+            new Token(tokenTypes.TOKEN_TYPE_ROUND_BRACKET, ')')
         ]);
 
         const tokens2 = lex('123\t\n\r  45  \t 6');
         expect(tokens2).toEqual([
             new Token(tokenTypes.TOKEN_TYPE_NUMBER, 123),
+            new Token(tokenTypes.TOKEN_TYPE_NEWLINE, '\n'),
             new Token(tokenTypes.TOKEN_TYPE_NUMBER, 45),
             new Token(tokenTypes.TOKEN_TYPE_NUMBER, 6),
         ]);
 
-        const tokens3 = lex('   "testString" \t\n\r  ');
+        const tokens3 = lex('   "testString" \t\r  ');
         expect(tokens3).toEqual([
             new Token(tokenTypes.TOKEN_TYPE_STRING, 'testString'),
         ]);
@@ -136,9 +132,21 @@ describe('Lexer Tests', () => {
         expect(tokens).toEqual([]);
     });
 
-    it('Should handle edge case of only whitespace', () => {
-        const tokens = lex(' \t\n\r ');
+    it('Should handle edge cases of only whitespace', () => {
+        let tokens = lex(' \t\n\r ');
+        expect(tokens).toEqual([
+            new Token(tokenTypes.TOKEN_TYPE_NEWLINE, '\n'),
+        ]);
+
+        tokens = lex(' \t\r ');
         expect(tokens).toEqual([]);
+        
+        tokens = lex(' \t \n\r\t\n  \n ');
+        expect(tokens).toEqual([
+            new Token(tokenTypes.TOKEN_TYPE_NEWLINE, '\n'),
+            new Token(tokenTypes.TOKEN_TYPE_NEWLINE, '\n'),
+            new Token(tokenTypes.TOKEN_TYPE_NEWLINE, '\n')
+        ]);
     })
 
     it('Should throw an error for invalid numbers', () => {
